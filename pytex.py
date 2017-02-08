cac15e8a58f4a8125afdf08cbf8c888bb0e18234
Robert Gonzalez <labratjazzcat@users.noreply.github.com>
2017-02-08 11:24:07 -0500

    Add files via upload

--- pytex.py
+++ pytex.py
@@ -2,51 +2,50 @@ from __future__ import print_function
 import sys
 
 OPCODES = ["S", "SS", "SSS", "BL", "EL", "BQ", "EQ", "IT", "FIG"]
-#file_name = sys.argv
-file_name = "input.txt"
+file_name = sys.argv
+#file_name = "input.txt"
 out = file_name.replace(".txt", ".tex")
-tex_file = open(out, 'w')
 text_file = open(file_name, 'r')
+sys.stdout = open(out, 'w')
 
-print("\\documentclass{article}", file=tex_file)
-print("\\pagestyle{plain}", file=tex_file)
-print("\\usepackage{graphicx, wrapfig, mathpazo, csquotes, float}\n", file=tex_file)
-print("\\begin{document}\n", file=tex_file)
-print("\\title{\\textbf{Title}}", file=tex_file)
-print("\\author{\\textsc{Author}}", file=tex_file)
-print("\\date{Date}", file=tex_file)
-print("\\maketitle\n", file=tex_file)
+print("\\documentclass{article}")
+print("\\pagestyle{plain}")
+print("\\usepackage{graphicx, wrapfig, mathpazo, csquotes, float}\n")
+print("\\begin{document}\n")
+print("\\title{\\textbf{Title}}")
+print("\\author{\\textsc{Author}}")
+print("\\date{Date}")
+print("\\maketitle\n")
 
 for line in text_file:
     row = line.split()
     if row[0] not in OPCODES:
-        print(line, file=tex_file)
+        print(line)
     else:
         if row[0] == OPCODES[0]:
-            print("\\section*{" + line[2:-1] + "}", file=tex_file)
+            print("\\section*{" + line[2:-1] + "}")
         elif row[0] == OPCODES[1]:
-            print("\\subsection*{" + line[3:-1] + "}", file=tex_file)
+            print("\\subsection*{" + line[3:-1] + "}")
         elif row[0] == OPCODES[2]:
-            print("\\subsubsection*{" + line[4:-1] + "}", file=tex_file)
+            print("\\subsubsection*{" + line[4:-1] + "}")
         elif row[0] == OPCODES[3]:
-            print("\\begin{itemize}", file=tex_file)
+            print("\\begin{itemize}")
         elif row[0] == OPCODES[4]:
-            print("\\end{itemize}", file=tex_file)
+            print("\\end{itemize}")
         elif row[0] == OPCODES[5]:
-            print("\\begin{displayquote}", file=tex_file)
+            print("\\begin{displayquote}")
         elif row[0] == OPCODES[6]:
-            print("\\end{displayquote}", file=tex_file)
+            print("\\end{displayquote}")
         elif row[0] == OPCODES[7]:
-            print("\\item " + line[3:-1], file=tex_file)
+            print("\\item " + line[3:-1])
         elif row[0] == OPCODES[8]:
-            print("\\begin{figure}[H]", file=tex_file)
-            print("  \\begin{center}", file=tex_file)
-            print("    \\includegraphics[width=0."+line[4]+"\\textwidth]{picture.png}", file=tex_file)
-            print("    \\caption{"+line[6:]+"}", file=tex_file)
-            print("    \\label{label}", file=tex_file)
-            print("  \\end{center}", file=tex_file)
-            print("\\end{figure}", file=tex_file)
+            print("\\begin{figure}[H]")
+            print("  \\begin{center}")
+            print("    \\includegraphics[width=0."+line[4]+"\\textwidth]{picture.png}")
+            print("    \\caption{"+line[6:]+"}")
+            print("    \\label{label}")
+            print("  \\end{center}")
+            print("\\end{figure}")
 
-print("\n\\end{document}", file=tex_file)
+print("\n\\end{document}")
 text_file.close()
-tex_file.close()
