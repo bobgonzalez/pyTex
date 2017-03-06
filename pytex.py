030184f4158aa745bd06157fe324aaf81254b7fe
labratjazzcat <labratjazzcat@outlook.com>
2017-03-06 14:15:16 -0500

    Added numbered sections and lists with suffix 'N',

--- pytex.py
+++ pytex.py
@@ -9,14 +9,13 @@ from datetime import date
     TODO: ability to add packages ", ".join[:-2]
     WISH_LIST_OF_MICROEXPRESSIONS:
          'B{' -> \textbf{} (unless '\B{')
-         BOL -> begin{enumerate}
-         EOOL -> end{enumerate}
          LINE x -> line across page where x = % of page width
+         BB + EB -> begin / end box
 
 
 """
 
-OPCODES = ["S", "SS", "SSS", "BL", "EL", "BQ", "EQ", "IT", "FIG"]
+OPCODES = ["S", "SS", "SSS", "SN", "SSN", "SSSN", "BL", "EL", "BQ", "EQ", "IT", "FIG", "BLN", "ELN"]
 file_name = sys.argv[1]
 #file_name = "input.txt"
 out = file_name.replace(".txt", ".tex")
@@ -39,24 +38,34 @@ for line in text_file:
     if row[0] not in OPCODES:
         print(line)
     else:
-        if row[0] == OPCODES[0]:
+        if row[0] == "S":
             print("\\section*{" + line[2:-1] + "}")
-        elif row[0] == OPCODES[1]:
+        elif row[0] == "SS":
             print("\\subsection*{" + line[3:-1] + "}")
-        elif row[0] == OPCODES[2]:
+        elif row[0] == "SSS":
             print("\\subsubsection*{" + line[4:-1] + "}")
-        elif row[0] == OPCODES[3]:
+        elif row[0] == "SN":
+            print("\\section{" + line[2:-1] + "}")
+        elif row[0] == "SSN":
+            print("\\subsection{" + line[3:-1] + "}")
+        elif row[0] == "SSSN":
+            print("\\subsubsection{" + line[4:-1] + "}")
+        elif row[0] == "BL":
             print("\\begin{itemize}")
-        elif row[0] == OPCODES[4]:
+        elif row[0] == "EL":
             print("\\end{itemize}")
-        elif row[0] == OPCODES[5]:
+        elif row[0] == "BLN":
+            print("\\begin{enumerate}")
+        elif row[0] == "ELN":
+            print("\\end{enumerate}")
+        elif row[0] == "BQ":
             print("\\begin{displayquote}")
-        elif row[0] == OPCODES[6]:
+        elif row[0] == "EQ":
             print("\\end{displayquote}")
-        elif row[0] == OPCODES[7]:
+        elif row[0] == "IT":
             print("\\item " + line[3].upper() + line[4:-1])
             #print("\\item " + line[3:-1])
-        elif row[0] == OPCODES[8]:
+        elif row[0] == "FIG":
             print("\\begin{figure}[H]")
             print("  \\begin{center}")
             print("    \\includegraphics[width=0." + row[1] +"\\textwidth]{" + row[2] + ".png}")
