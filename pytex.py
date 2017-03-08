e9ce937fbe813295d5c7fca57ee919e594e39a63
labratjazzcat <labratjazzcat@outlook.com>
2017-03-07 21:20:20 -0500

    resolved merge inconsistencies

--- pytex.py
+++ pytex.py
@@ -17,30 +17,89 @@ from subprocess import call
          TLINE -> \\ \hline
          BT + ET -> begin table center tabular end where BT takes in '|l|l|' paramenter to tabular
 """
-if(sys.argv[1]=='-h' or '--help')
-    help()
-else
-    regular()
-def help():
-    print("S - Subscetion \n
-          SS - SubSection \n
-          SSS - SubSubSection\n
-          SN - Section Numbered\n
-          SSN - SubSection Numbered\n
-          SSSN - SubSubSection Numbered\n
-          BL - Begin List\n
-          EL - End List\n
-          BLN - Begin List Numbered\n
-          ELN - End List Numbered\n
-          BQ - Begin Text Isolation\n
-          EQ - End Text Isolation\n
-          BB - Begin Drawing A Box Around Text\n
-          EB - End Drawing A Box Aroung Text\n
-          IT - Item\n
-          FIG - 3 Parameters\n
-          \t percentage of width of page < 1, Title of Picture,Caption\n
-          LINE - Draw A Line Across The Page"
-          
+
+
+def help_me():
+    print("S - Subscetion \nSS - SubSection \nSSS - SubSubSection\n" +
+          "SN - Section Numbered\nSSN - SubSection Numbered\nSSSN - SubSubSection Numbered\n" +
+          "BL - Begin List\nEL - End List\nBLN - Begin List Numbered\nELN - End List Numbered\n" +
+          "BQ - Begin Text Isolation\nEQ - End Text Isolation\nBB - Begin Drawing A Box Around Text\n" +
+          "EB - End Drawing A Box Aroung Text\nIT - Item\nFIG - 3 Parameters\n" +
+          "\t percentage of width of page < 1, Title of Picture,Caption\nLINE - Draw A Line Across The Page")
+
+
+def init():
+    OPCODES = ["S", "SS", "SSS", "SN", "SSN", "SSSN", "BL", "EL", "BQ", "EQ", "BB", "EB", "IT", "FIG", "BLN", "ELN", "LINE"]
+    file_name = sys.argv[1]
+    out = file_name.replace(".txt", ".tex")
+    text_file = open(file_name, 'r')
+    sys.stdout = open(out, 'w')
+    cur_date = date.today()
+    file_name = file_name.replace("_", "\\_")
+
+    print("\\documentclass{article}")
+    print("\\pagestyle{plain}")
+    print("\\usepackage{graphicx, wrapfig, mathpazo, csquotes, float, tcolorbox}\n")
+    print("\\begin{document}\n")
+    print("\\title{\\textbf{" + file_name[:-4] + "}}")
+    print("\\author{\\textsc{}}")
+    print("\\date{" + str(cur_date.month) + "/" + str(cur_date.day) + "/" + str(cur_date.year) + "}")
+    print("\\maketitle\n")
+
+    for line in text_file:
+        row = line.split()
+        if row[0] not in OPCODES:
+            print(token_check(line))
+        else:
+            if row[0] == "S":
+                print("\\section*{" + line[2:-1] + "}")
+            elif row[0] == "SS":
+                print("\\subsection*{" + line[3:-1] + "}")
+            elif row[0] == "SSS":
+                print("\\subsubsection*{" + line[4:-1] + "}")
+            elif row[0] == "SN":
+                print("\\section{" + line[2:-1] + "}")
+            elif row[0] == "SSN":
+                print("\\subsection{" + line[3:-1] + "}")
+            elif row[0] == "SSSN":
+                print("\\subsubsection{" + line[4:-1] + "}")
+            elif row[0] == "BL":
+                print("\\begin{itemize}")
+            elif row[0] == "EL":
+                print("\\end{itemize}")
+            elif row[0] == "BLN":
+                print("\\begin{enumerate}")
+            elif row[0] == "ELN":
+                print("\\end{enumerate}")
+            elif row[0] == "BQ":
+                print("\\begin{displayquote}")
+            elif row[0] == "EQ":
+                print("\\end{displayquote}")
+            elif row[0] == "BB":
+                print("\\begin{tcolorbox}")
+            elif row[0] == "EB":
+                print("\\end{tcolorbox}")
+            elif row[0] == "IT":
+                l = token_check(line)
+                if l[3] == '\\':
+                    print("\\item " + l[3:11] + l[11].upper() + l[12:])
+                else:
+                    print("\\item " + l[3].upper() + l[4:])
+                #print("\\item " + line[3:-1])
+            elif row[0] == "FIG":
+                print("\\begin{figure}[H]")
+                print("  \\begin{center}")
+                print("    \\includegraphics[width=0." + row[1] +"\\textwidth]{" + row[2] + ".png}")
+                print("    \\caption{" + ' '.join(row[3:]) + "}")
+                print("    \\label{label}")
+                print("  \\end{center}")
+                print("\\end{figure}")
+            elif row[0] == "LINE":
+                print("\\rule{\\linewidth}{0.2mm}")
+    print("\n\\end{document}")
+    text_file.close()
+    sys.stdout = sys.__stdout__
+    call(["pdflatex", str(out)])
 
 
 def token_check(line1):
@@ -51,75 +110,8 @@ def token_check(line1):
     new_line = ' '.join(sp_line)
     return new_line
 
-def regular():
-OPCODES = ["S", "SS", "SSS", "SN", "SSN", "SSSN", "BL", "EL", "BQ", "EQ", "BB", "EB", "IT", "FIG", "BLN", "ELN", "LINE"]
-file_name = sys.argv[1]
-out = file_name.replace(".txt", ".tex")
-text_file = open(file_name, 'r')
-sys.stdout = open(out, 'w')
-cur_date = date.today()
-file_name = file_name.replace("_", "\\_")
 
-print("\\documentclass{article}")
-print("\\pagestyle{plain}")
-print("\\usepackage{graphicx, wrapfig, mathpazo, csquotes, float, tcolorbox}\n")
-print("\\begin{document}\n")
-print("\\title{\\textbf{" + file_name[:-4] + "}}")
-print("\\author{\\textsc{}}")
-print("\\date{" + str(cur_date.month) + "/" + str(cur_date.day) + "/" + str(cur_date.year) + "}")
-print("\\maketitle\n")
-
-for line in text_file:
-    row = line.split()
-    if row[0] not in OPCODES:
-        print(token_check(line))
-    else:
-        if row[0] == "S":
-            print("\\section*{" + line[2:-1] + "}")
-        elif row[0] == "SS":
-            print("\\subsection*{" + line[3:-1] + "}")
-        elif row[0] == "SSS":
-            print("\\subsubsection*{" + line[4:-1] + "}")
-        elif row[0] == "SN":
-            print("\\section{" + line[2:-1] + "}")
-        elif row[0] == "SSN":
-            print("\\subsection{" + line[3:-1] + "}")
-        elif row[0] == "SSSN":
-            print("\\subsubsection{" + line[4:-1] + "}")
-        elif row[0] == "BL":
-            print("\\begin{itemize}")
-        elif row[0] == "EL":
-            print("\\end{itemize}")
-        elif row[0] == "BLN":
-            print("\\begin{enumerate}")
-        elif row[0] == "ELN":
-            print("\\end{enumerate}")
-        elif row[0] == "BQ":
-            print("\\begin{displayquote}")
-        elif row[0] == "EQ":
-            print("\\end{displayquote}")
-        elif row[0] == "BB":
-            print("\\begin{tcolorbox}")
-        elif row[0] == "EB":
-            print("\\end{tcolorbox}")
-        elif row[0] == "IT":
-            l = token_check(line)
-            if l[3] == '\\':
-                print("\\item " + l[3:11] + l[11].upper() + l[12:])
-            else:
-                print("\\item " + l[3].upper() + l[4:])
-            #print("\\item " + line[3:-1])
-        elif row[0] == "FIG":
-            print("\\begin{figure}[H]")
-            print("  \\begin{center}")
-            print("    \\includegraphics[width=0." + row[1] +"\\textwidth]{" + row[2] + ".png}")
-            print("    \\caption{" + ' '.join(row[3:]) + "}")
-            print("    \\label{label}")
-            print("  \\end{center}")
-            print("\\end{figure}")
-        elif row[0] == "LINE":
-            print("\\rule{\\linewidth}{0.2mm}")
-print("\n\\end{document}")
-text_file.close()
-sys.stdout = sys.__stdout__
-call(["pdflatex", str(out)])
+if sys.argv[1] == '-h' or sys.argv[1] == '--help':
+    help_me()
+else:
+    init()
