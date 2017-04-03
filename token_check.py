e339c79481191267068d4204ddf0fe7c3a863971
labratjazzcat <labratjazzcat@outlook.com>
2017-04-03 16:40:40 -0400

    fixed arrows

--- token_check.py
+++ token_check.py
@@ -15,20 +15,20 @@ def token_check(line1):
             sp_line[i] = "$\\rightarrow$"
         elif token[0:3] == "-->":
             sp_line[i] = "$\\longrightarrow$"
-        elif token[0:2] == "<-":
-            sp_line[i] = "$\\leftarrow$"
         elif token[0:3] == "<--":
             sp_line[i] = "$\\longleftarrow$"
+        elif token[0:2] == "<-":
+            sp_line[i] = "$\\leftarrow$"
         elif token[0:2] == "=>":
             sp_line[i] = "$\\Rightarrow$"
         elif token[0:3] == "==>":
             sp_line[i] = "$\\Longrightarrow$"
         elif token[0:3] == "<=>":
             sp_line[i] = "$\\Leftrightarrow$"
-        elif token[0:2] == "<=":
-            sp_line[i] = "$\\Leftarrow$"
         elif token[0:3] == "<==":
             sp_line[i] = "$\\Longleftarrow$"
+        elif token[0:2] == "<=":
+            sp_line[i] = "$\\Leftarrow$"
         elif token[0:3] == "|->":
             sp_line[i] = "$\\mapsto$"
 
