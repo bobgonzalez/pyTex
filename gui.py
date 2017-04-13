e90b9aa7a87e72cf17589f2f0bfc7aebc02620a1
labratjazzcat <labratjazzcat@outlook.com>
2017-04-12 20:33:39 -0400

    added vertical and horizontal scrollbars, need to be placed correctly. added skeletons for soom in / out and mouse drag

--- gui.py
+++ gui.py
@@ -58,8 +58,29 @@ class Redirector(object):
         button.grid(row=8,column=0,sticky=E+W)
         button = Button(self.parent, text="Terminal", command=self.term)
         button.grid(row=8,column=1,sticky=E+W)
-        #button = Button(self.parent, text="Spell Check", command=self.spell)
-        #button.grid(row=5,column=2,sticky=E+W)
+        self.canvas = Canvas(self.parent, width=50, height=80, scrollregion=(0,0,1600,2000))
+        self.canvas.grid(row=0, column=3, rowspan=8, columnspan=7, sticky=W + E + N + S)
+        #self.canvas.config(scrollregion=self.canvas.bbox(ALL))
+        self.scale = 1.0
+        self.orig_img = None
+        self.img = None
+        self.zoomcycle = 0
+        self.zimg_id = None
+        self.hbar = Scrollbar(self.canvas, orient=HORIZONTAL)
+        self.hbar.grid(sticky = E, row = 8, column = 5, rowspan = 1, columnspan = 6)
+        self.hbar.config(command=self.canvas.xview)
+        self.vbar = Scrollbar(self.canvas, orient=VERTICAL)
+        self.vbar.grid(sticky = E+S+W, row = 0, column = 9, rowspan = 8, columnspan = 1)
+        self.vbar.config(command=self.canvas.yview)
+        self.canvas.config(width=50, height=80)
+        self.canvas.config(xscrollcommand=self.hbar.set, yscrollcommand=self.vbar.set)
+        b1 = Button(root, text=">", command=self.show_image1)
+        b1.grid(row=8, column=7, sticky=E + W)
+        b2 = Button(root, text="<", command=self.show_image2)
+        b2.grid(row=8, column=6, sticky=E + W)
+
+        #root.bind("<MouseWheel>", self.zoomer)
+        #self.canvas.bind("<Motion>", self.crop)
 
     def main(self, *args):
         self.text_box = Text(self.parent, bd=5)
@@ -75,11 +96,31 @@ class Redirector(object):
         pdf2jpg(reverse[:-3]+"pdf")
         global img_list
         img_list = []
+        resize(1600,2000)
         img_list = get_jpegs()
+        self.canvas.delete("all")
+        self.orig_img = img_list[counter]
+        self.img = ImageTk.PhotoImage(self.orig_img)
+        self.canvas.create_image(0, 0, image=self.img, anchor="nw")
+
+        self.vbar = Scrollbar(self.canvas, orient=VERTICAL)
+        self.vbar.grid(sticky=E + N + S, row=0, column=9, rowspan=8, columnspan=1)
+        self.vbar.config(command=self.canvas.yview)
+        self.canvas.config(width=50, height=80)
+        self.canvas.config(yscrollcommand=self.vbar.set)
+        #self.canvas.image = self.img
+        '''
+        canvas.delete("all")
+        self.orig_img = img_list[counter]
+        self.img = ImageTk.PhotoImage(self.orig_img.resize((1200, 1600), Image.NEAREST))
+        canvas.create_image(0, 0, image=self.img, anchor="nw")
+        canvas.image = self.img
+
         canvas.delete("all")
         image2 = ImageTk.PhotoImage(img_list[counter].resize((1200, 1600), Image.NEAREST))
         canvas.create_image(0, 0, anchor='nw', image=image2)
         canvas.image = image2
+        '''
 
     def main_help(self):
         print help_me()
@@ -112,6 +153,57 @@ class Redirector(object):
         wid = self.text_box.winfo_id()
         os.system('xterm -into %d -geometry 400x500 -sb &' % wid)
 
+    def show_image1(self):
+        """ > button """
+        self.canvas.delete("all")
+        image1 = ImageTk.PhotoImage(img_list[counter])
+        self.canvas.create_image(0,0, anchor='nw',image=image1)
+        self.canvas.image = image1
+        global counter
+        if counter == len(img_list)-1:
+            counter = 0
+        else:
+            counter = counter + 1
+        #print "in > button, counter is: " , counter
+
+    def show_image2(self):
+        """ < button """
+        self.canvas.delete("all")
+        #image2 = ImageTk.PhotoImage(img_list[counter].resize((800, 1100), Image.NEAREST))
+        image2 = ImageTk.PhotoImage(img_list[counter])
+        self.canvas.create_image(0,0, anchor='nw',image=image2)
+        self.canvas.image = image2
+        global counter
+        if counter == 0:
+            counter = len(img_list)-1
+        else:
+            counter = counter - 1
+        #print "in < button, counter is: " , counter
+
+'''
+    def zoomer(self, event):
+        if (event.delta > 0):
+            if self.zoomcycle != 4: self.zoomcycle += 1
+        elif (event.delta < 0):
+            if self.zoomcycle != 0: self.zoomcycle -= 1
+        self.crop(event)
+
+    def crop(self, event):
+        if self.zimg_id: self.canvas.delete(self.zimg_id)
+        if (self.zoomcycle) != 0:
+            x, y = event.x, event.y
+            if self.zoomcycle == 1:
+                tmp = self.orig_img.crop((x - 45, y - 30, x + 45, y + 30))
+            elif self.zoomcycle == 2:
+                tmp = self.orig_img.crop((x - 30, y - 20, x + 30, y + 20))
+            elif self.zoomcycle == 3:
+                tmp = self.orig_img.crop((x - 15, y - 10, x + 15, y + 10))
+            elif self.zoomcycle == 4:
+                tmp = self.orig_img.crop((x - 6, y - 4, x + 6, y + 4))
+            size = 30, 20
+            self.zimg = ImageTk.PhotoImage(tmp.resize(size))
+            self.zimg_id = self.canvas.create_image(event.x, event.y, image=self.zimg)
+'''
 
 def callback(*args):
     name = askopenfilename()
@@ -134,33 +226,6 @@ def open_exp(*args):
 counter = 0
 
 
-def show_image1():
-    """ > button """
-    canvas.delete("all")
-    image1 = ImageTk.PhotoImage(img_list[counter])
-    canvas.create_image(0,0, anchor='nw',image=image1)
-    canvas.image = image1
-    global counter
-    if counter == len(img_list)-1:
-        counter = 0
-    else:
-        counter = counter + 1
-    #print "in > button, counter is: " , counter
-
-
-def show_image2():
-    """ < button """
-    canvas.delete("all")
-    #image2 = ImageTk.PhotoImage(img_list[counter].resize((800, 1100), Image.NEAREST))
-    image2 = ImageTk.PhotoImage(img_list[counter])
-    canvas.create_image(0,0, anchor='nw',image=image2)
-    canvas.image = image2
-    global counter
-    if counter == 0:
-        counter = len(img_list)-1
-    else:
-        counter = counter - 1
-    #print "in < button, counter is: " , counter
 
 
 def file_save(*args):
@@ -190,16 +255,16 @@ root.config(menu=menu)
 fileMenu = Menu(menu)
 editMenu = Menu(menu)
 
-canvas = Canvas(height=500, width=800)
+#canvas = Canvas(height=500, width=800)
 
 for r in range(10):
     root.rowconfigure(r, weight=1)
 for c in range(10):
     root.columnconfigure(c, weight=1)
 
-canvas.grid(row = 0, column = 3, rowspan = 8, columnspan = 7, sticky = W+E+N+S)
-scroll_bar_left = Scrollbar(root, orient="vertical", command=canvas.yview)
-scroll_bar_left.grid(sticky = E+N+S, row = 0, column = 9, rowspan = 8, columnspan = 1)
+#canvas.grid(row = 0, column = 3, rowspan = 8, columnspan = 7, sticky = W+E+N+S)
+#scroll_bar_left = Scrollbar(root, orient="vertical", command=canvas.yview)
+#scroll_bar_left.grid(sticky = E+N+S, row = 0, column = 9, rowspan = 8, columnspan = 1)
 
 menu.add_cascade(label="File", menu=fileMenu)
 fileMenu.add_command(label="Open", command=callback, accelerator="Ctrl+o")
@@ -215,10 +280,7 @@ root.bind_all("<Control-t>", gui.term)
 root.bind_all("<Control-s>", file_save)
 global img_list
 
-b1 = Button(root, text=">", command=show_image1)
-b1.grid(row=8,column=7,sticky=E+W)
-b2 = Button(root, text="<", command=show_image2)
-b2.grid(row=8,column=6,sticky=E+W)
+
 #b2 = Button(root, text="term", command=term)
 #b2.grid(row=8,column=4,sticky=E+W)
 
