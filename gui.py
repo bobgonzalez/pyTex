98a68fcdad761f7232e06b7b4006618833f95ac9
labratjazzcat <labratjazzcat@outlook.com>
2017-04-18 21:53:41 -0400

    updated TODOs, added status bar, added make title option.

--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ from tkFileDialog import askopenfilename
 from PIL import Image, ImageTk  #sudo apt-get install python-imaging-tk
 from expand_ptex import init as compile_me
 from help_me import help_me
-import ttk
+#import ttk
 import tkFileDialog
 from img_proc import *
 import os
@@ -12,57 +12,52 @@ import distutils.dir_util
 
 
 """
-    *TODO: add check box for optional make title
-    *TODO: move from .pack() to .grid()
-    *TODO: zoom buttons for viewer frame
-    *TODO: implement file save
-    *TODO: redirect compile to buffer window
-    *TODO: split buffer frame into buffer 75% left help 25% right
-    *TODO: add 'spell check' button that runs aspell in buffer frame
+    *TODO: add pop up window asking for title and author when make_title is selected
+    *TODO: add ability for user to change their default zlevel
 """
-# s = ttk.Style()
-# s.theme_use('alt')
 
-"""
+TITLE = False
+twoT = 0
+
+
 class StatusBar(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
         self.variable = StringVar()
-        self.label = Label(self, bd=1, relief=SUNKEN, anchor=W, textvariable=self.variable, font=('arial', 16, 'normal'))
+        self.label = Label(self, bd=1, relief=SUNKEN, anchor=W, textvariable=self.variable, font=('arial', 10, 'normal'))
         self.variable.set('Status Bar')
-        self.label.grid(row=6)
-        self.grid(row=6, column=0, sticky=S)
-"""
+        self.label.grid(row=20, column=0, columnspan=10, sticky=W+E)
+        self.grid(row=20, column=0, columnspan=10, sticky=W+E)
 
 
 class Redirector(object):
     def __init__(self, parent):
-        global twoT
-        twoT = 0
         self.parent = parent
+        self.status = StatusBar(parent)
         self.input_f = ''
         self.x = 1600
         self.y = 2000
         self.img_list = []
         self.counter = 0
         self.zlevel = 0 #Zoom level [-5,5]
+        self.default_status()
         button = Button(self.parent, text="Compile", command=self.comp)
-        button.grid(row=9, column=0, sticky=E+W)
+        button.grid(row=19, column=0, sticky=E+W)
         button = Button(self.parent, text="Help", command=self.help)
-        button.grid(row=9, column=1, sticky=E+W)
+        button.grid(row=19, column=1, sticky=E+W)
         button = Button(self.parent, text="+", command=self.zoom_in)
-        button.grid(row=9, column=4, sticky=E+W)
+        button.grid(row=19, column=4, sticky=E+W)
         button = Button(self.parent, text="-", command=self.zoom_out)
-        button.grid(row=9, column=3, sticky=E+W)
+        button.grid(row=19, column=3, sticky=E+W)
         self.canvas = Canvas(self.parent, width=50, height=80, scrollregion=(0, 0, self.x, self.y))
-        self.canvas.grid(row=0, column=3, rowspan=9, columnspan=7, sticky=W+E+N+S)
+        self.canvas.grid(row=0, column=3, rowspan=19, columnspan=7, sticky=W+E+N+S)
         #self.canvas.config(scrollregion=self.canvas.bbox(ALL))
         self.orig_img = None
         self.img = None
         b1 = Button(root, text=str(">"), command=self.show_image1)
-        b1.grid(row=9, column=7, sticky=E+W)
+        b1.grid(row=19, column=7, sticky=E+W)
         b2 = Button(root, text="<", command=self.show_image2)
-        b2.grid(row=9, column=6, sticky=E+W)
+        b2.grid(row=19, column=6, sticky=E+W)
         self.canvas.bind("<ButtonPress-1>", self.scroll_start)
         self.canvas.bind("<B1-Motion>", self.scroll_move)
         self.canvas.bind("<Control-Button-4>", self.wheel_zoom)
@@ -79,7 +74,10 @@ class Redirector(object):
         distutils.dir_util.mkpath('./Zoom')
         if twoT == 1:
             file_save2()
-        print compile_me(self.input_f)
+        if TITLE:
+            print compile_me(self.input_f, 1, "Title", "Author")
+        else:
+            print compile_me(self.input_f)
         reverse = self.input_f[::-1]
         a = reverse.index('/')
         reverse = reverse[:a]
@@ -90,11 +88,10 @@ class Redirector(object):
         resize(self.x, self.y)
         self.img_list = get_jpegs()
         self.canvas.delete("all")
-        #if len(self.img_list) > 1:
-        #    self.counter = len(self.img_list) - 1
         self.orig_img = self.img_list[self.counter]
         self.img = ImageTk.PhotoImage(self.orig_img)
         self.canvas.create_image(0, 0, image=self.img, anchor="nw")
+        self.status.variable.set('Compiled ' + self.input_f)
 
     def main_help(self):
         print help_me()
@@ -110,7 +107,7 @@ class Redirector(object):
         self.orig_img = self.img_list[self.counter]
         self.img = ImageTk.PhotoImage(self.orig_img)
         self.canvas.create_image(0, 0, image=self.img, anchor="nw")
-
+        self.default_status()
     def zoom_out(self, *args):
         if self.zlevel > -9: #Zoom in
             self.zlevel -= 1
@@ -122,7 +119,7 @@ class Redirector(object):
         self.orig_img = self.img_list[self.counter]
         self.img = ImageTk.PhotoImage(self.orig_img)
         self.canvas.create_image(0, 0, image=self.img, anchor="nw")
-
+        self.default_status()
     def wheel_zoom(self, event):
         if event.num == 4 and self.zlevel < 9: #Zoom in
             self.zlevel += 1
@@ -136,6 +133,7 @@ class Redirector(object):
         self.orig_img = self.img_list[self.counter]
         self.img = ImageTk.PhotoImage(self.orig_img)
         self.canvas.create_image(0, 0, image=self.img, anchor="nw")
+        self.default_status()
 
     def help(self, *args):
         x = "this is a place holder"
@@ -151,6 +149,7 @@ class Redirector(object):
         self.orig_img = self.img_list[self.counter]
         self.img = ImageTk.PhotoImage(self.orig_img)
         self.canvas.create_image(0, 0, image=self.img, anchor="nw")
+        self.default_status()
 
     def show_image2(self, *args):
         """ < button """
@@ -163,7 +162,12 @@ class Redirector(object):
         self.orig_img = self.img_list[self.counter]
         self.img = ImageTk.PhotoImage(self.orig_img)
         self.canvas.create_image(0, 0, image=self.img, anchor="nw")
+        self.default_status()
 
+    def default_status(self):
+        self.status.variable.set('Current Zoom Level: ' + str(self.zlevel) +
+                                 ' \tCurrent Page: ' + str(self.counter + 1) +
+                                 ' \tMake Title: ' + str(TITLE))
 
 def file_open(*args):
     name = askopenfilename()
@@ -205,15 +209,16 @@ def two_terms(*args):
     global twoT
     global L1
     if twoT == 1:
-        file_save2()
+        if gui.input_f != '':
+            file_save2()
         t_box = Frame(root, height=60, width=40)
-        t_box.grid(row=0, column=0, rowspan=9, columnspan=3, sticky=W+E+N+S)
+        t_box.grid(row=0, column=0, rowspan=19, columnspan=3, sticky=W+E+N+S)
         wid = t_box.winfo_id()
         os.system('xterm -into %d -geometry 400x500 -sb &' % wid)
         twoT = 0
     else:
         L1 = Text(root, bd=5)
-        L1.grid(row=0, column=0, rowspan=9, columnspan=3, sticky=W + E + N + S)
+        L1.grid(row=0, column=0, rowspan=19, columnspan=3, sticky=W + E + N + S)
         twoT = 1
         if gui.input_f != '':
             L1.delete('1.0', END)
@@ -222,17 +227,26 @@ def two_terms(*args):
             L1.insert(INSERT, content)
 
 
+def make_title(*args):
+    global TITLE
+    if TITLE:
+        TITLE = False
+    else:
+        TITLE = True
+    gui.default_status()
+
+
 root = Tk()
 root.title("pyTex")
 root.grid()
 menu = Menu(root)
-#d = StatusBar(root)
+
 gui = Redirector(root)
 root.config(menu=menu)
 fileMenu = Menu(menu)
 editMenu = Menu(menu)
 
-for r in range(10):
+for r in range(11):
     root.rowconfigure(r, weight=1)
 for c in range(10):
     root.columnconfigure(c, weight=1)
@@ -245,7 +259,8 @@ fileMenu.add_separator()
 fileMenu.add_command(label="Exit", command=root.quit)
 menu.add_cascade(label="Edit", menu=editMenu)
 editMenu.add_command(label="Micro-Exps", command=open_exp)
-editMenu.add_command(label="2-terms", command=two_terms, accelerator="Ctrl+r")
+editMenu.add_command(label="Make Title", command=make_title)
+editMenu.add_command(label="Use Terminal", command=two_terms, accelerator="Ctrl+r")
 root.bind_all("<Control-o>", file_open)
 root.bind_all("<Control-c>", gui.comp)
 root.bind_all("<Control-h>", gui.help)
@@ -259,6 +274,7 @@ root.bind_all("<Alt-Left>", gui.show_image2)
 root.bind_all("<Alt-Right>", gui.show_image1)
 root.bind_all("<Alt-Up>", gui.zoom_in)
 root.bind_all("<Alt-Down>", gui.zoom_out)
+L1 = Text(root, bd=5)
 two_terms()
 
 '''open window'''
