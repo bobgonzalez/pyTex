70dba47f095ce3005c33f3a5b1645055fe123574
labratjazzcat <labratjazzcat@outlook.com>
2017-04-18 20:47:09 -0400

    Updated help function

--- help_me.py
+++ help_me.py
@@ -1,33 +1,47 @@
 def help_me():
-    return("___________________________________________________\n" +
-          "*The micro-expressions that follow must \n" +
-          " be the first word on the line to be expanded\n" +
-          "---------------------------------------------------\n" +
-          "S  \tSection \n" +
-          "SS \tSubSection \n" +
-          "SSS \tSubSubSection\n" +
-          "SN \tSection Numbered\n" +
-          "SSN \tSubSection Numbered\n" +
-          "SSSN \tSubSubSection Numbered\n" +
-          "BL \tBegin List\n" +
-          "EL \tEnd List\n" +
-          "BLN \tBegin List Numbered\n" +
-          "ELN \tEnd List Numbered\n" +
-          "BQ \tBegin Text Isolation\n" +
-          "EQ \tEnd Text Isolation\n" +
-          "BB \tBegin Drawing A Box Around Text\n" +
-          "EB \tEnd Drawing A Box Around Text\n" +
-          "IT \tItem\n" +
-          "LINE \tDraw A Line Across Page\n" +
-          "FIG \t3 Parameters, space separated\n" +
-          "\t\t% of Page-Width < 1, \n" +
-          "\t\tPicture Name (assumes .png), \n" +
-          "\t\tCaption\n" +
-          "___________________________________________________\n" +
-          "*The micro-expressions that follow may \n" +
-          " be any word on the line and will be expanded\n" +
-          "---------------------------------------------------\n" +
-          "B{ \tBolds all text till closing },\n" +
-          "\tmust have a space preceding it\n" +
-          "___________________________________________________"
-          )
+    return("_________________________________________________________________\n" +
+           "*The micro-expressions that follow must be the first word on \n" +
+           " the line to be expanded\n" +
+           "-----------------------------------------------------------------\n" +
+           "  IG  \tIgnore first-word micro-expression on this line\n" +
+           "  IGA  \tIgnore all micro-expressions on this line\n" +
+           "  S  \tSection \n" +
+           "  SS \tSubSection \n" +
+           "  SSS \tSubSubSection\n" +
+           "  SN \tSection Numbered\n" +
+           "  SSN \tSubSection Numbered\n" +
+           "  SSSN \tSubSubSection Numbered\n" +
+           "  BL \tBegin List\n" +
+           "  IT \tItem\n" +
+           "  EL \tEnd List\n" +
+           "  BLN \tBegin List Numbered\n" +
+           "  ELN \tEnd List Numbered\n" +
+           "  BQ \tBegin Quote Indentation\n" +
+           "  EQ \tEnd Quote Indentation\n" +
+           "  BT \tBegin Theorem\n" +
+           "  ET \tEnd Theorem\n" +
+           "  BP \tBegin Proof\n" +
+           "  EP \tEnd Proof\n" +
+           "  BB \tBegin Drawing A Box Around Text\n" +
+           "  EB \tEnd Drawing A Box Around Text\n" +
+           "  LINE \tDraw A Line Across Page\n" +
+           "  FIG \t3 Parameters, space separated\n" +
+           "  \t\t% of Page-Width < 1, \n" +
+           "  \t\tPicture Name (assumes .png), \n" +
+           "  \t\tCaption\n" +
+           "_________________________________________________________________\n" +
+           "*The micro-expressions that follow must be any\n" +
+           " word proceeded by a space to be expanded\n" +
+           "-----------------------------------------------------------------\n" +
+           "  B{ \tBolds all text till closing \'}\'\n" +
+           "  I{ \tItalicizes all text till closing \'}\'\n" +
+           "  -> \tRight Arrow\n" +
+           "  --> \tLong Right Arrow\n" +
+           "  <- \tLeft Arrow\n" +
+           "  <-- \tLong Left Arrow\n" +
+           "  => \tDouble Right Arrow\n" +
+           "  <= \tDouble Left Arrow\n" +
+           "  <=> \tDouble Left Right Arrow\n" +
+           "  |-> \tMapsto Arrow\n" +
+           "_________________________________________________________________"
+           )
